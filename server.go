2f55e24770ec10aa309f91b9b94121ea303e3207
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-03-02 18:29:35 -0500

    Router created

--- server.go
+++ server.go
@@ -1,21 +1,45 @@
-package main // Conoce todo lo de este paquete
-import "net/http"
+//el archivo main va a poder leer todo lo que este en el archivo server
+package main
 
-// Puerto del servidor
+import (
+	"net/http"
+)
+
 type Server struct {
+	//puerto del servidor para escuchar las conexiones
 	port string
+
+	//se agrega el atributo router que es un apuntador al struct Router de router.go
+	router *Router
 }
 
-// Instanciar servidor y escuchar conexciones
+//Funcion tipo global para ser leida en otros archivos
+//Sirve para instanciar el servidor y que sea capaz de escuchar las conexiones
+//recive el puerto que tiene que estar escuchando y devuelve el servidor como tal
 func NewServer(port string) *Server {
 	return &Server{
 		port: port,
+
+		//router instanciado
+		router: NewRouter(),
+		//con esto el servidor ya es capaz de instanciar el router y de tenerlo como propiedad
 	}
 }
 
+//Funcion tipo receiver, del struct Server, devuelve un error en caso de que haya problemas al conectar
 func (s *Server) Listen() error {
-	// Seguno parametro es un handler
+	//el router va a ser el encargado de tomar las urls y procesarlas como se debe, crea el entry-point
+	// los parametro son: el slash que es el punto de entrada, y el handler es el router recien creado
+	http.Handle("/", s.router)
+
+	//con la funcion listenanserve() del paquete http nos ayuda a escuchar todas las peticiones
+	//colocas el puerto como primer parametro, el segundo es un handler
+	//pero nosotros haremos nuestros handlers por eso se coloca nil
 	err := http.ListenAndServe(s.port, nil)
 
-	return err
+	if err != nil {
+		return err
+	}
+	//si la ejecucion salio bien, retorna un valor nil
+	return nil
 }
